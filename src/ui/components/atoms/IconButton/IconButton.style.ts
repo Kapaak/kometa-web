49ff4e03aed67e305d2be5e43aafb0a784a04d63
Pavel Zapletal <pavel.zaplet25@gmail.com>
2024-05-18 17:53:24 +0200

    fix: button with icon transform animation

--- src/ui/components/atoms/IconButton/IconButton.style.ts
+++ src/ui/components/atoms/IconButton/IconButton.style.ts
@@ -1,5 +1,19 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-import { Button } from '../Button'
+import { Button } from '../Button';
 
-export const IconButton = styled(Button)``
+export const IconButton = styled(Button)`
+  svg {
+    transform: translateX(0);
+    transition: transform 0.2s;
+  }
+
+  &:hover {
+    background-color: initial;
+
+    svg {
+      transform: translateX(0.4rem);
+      transition: transform 0.2s;
+    }
+  }
+`;
