0472768a27d4ac99b4831716159e34da530136b9
Pavel Zapletal <pavel.zaplet25@gmail.com>
2024-05-30 17:16:58 +0200

    fix: radix-ui select

--- src/ui/components/atoms/Select/Select.tsx
+++ src/ui/components/atoms/Select/Select.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useRef, useState } from 'react';
+import { useMemo, useState } from 'react';
 
 import { ArrowUp, CaretDown } from '@phosphor-icons/react/dist/ssr';
 import * as RadixUiSelect from '@radix-ui/react-select';
@@ -27,12 +27,12 @@ export const Select = ({
   onChange,
   value,
 }: SelectProps) => {
+  const [open, setOpen] = useState(false);
   const [selectedValue, setSelectedValue] = useState(value ?? '');
-
-  const handleOpenChange = useSelectInteractionFix('#__next');
 
   const handleValueChange = (value: string) => {
     onChange?.(value);
+    setOpen(false);
     setSelectedValue(value);
   };
 
@@ -44,71 +44,46 @@ export const Select = ({
 
   return (
     <S.Select
+      open={open}
       onValueChange={handleValueChange}
       value={selectedValue}
-      onOpenChange={handleOpenChange}
     >
-      <S.SelectTrigger>
+      <S.SelectTrigger onClick={() => setOpen(true)}>
         <S.SelectValue placeholder={<Text variant="body5">{placeholder}</Text>}>
           <Text variant="body5">{displayValue}</Text>
         </S.SelectValue>
         <CaretDown />
       </S.SelectTrigger>
       <RadixUiSelect.Portal>
-        <S.SelectContent position="popper">
+        <S.SelectContent
+          position="popper"
+          onPointerDownOutside={() => {
+            setOpen(false);
+          }}
+        >
           <RadixUiSelect.ScrollUpButton>
             <ArrowUp />
           </RadixUiSelect.ScrollUpButton>
           <S.SelectViewport>
-            {options?.map((option) => (
-              <SelectItem
-                key={option.value}
-                value={option.value}
-                checked={selectedValue === option.value}
-              >
-                {option.label}
-              </SelectItem>
-            ))}
+            <RadixUiSelect.Group
+              ref={(ref) =>
+                ref?.addEventListener('touchend', (e) => e.preventDefault())
+              }
+            >
+              {options?.map((option) => (
+                <SelectItem
+                  key={option.value}
+                  value={option.value}
+                  checked={selectedValue === option.value}
+                >
+                  {option.label}
+                </SelectItem>
+              ))}
+            </RadixUiSelect.Group>
           </S.SelectViewport>
           <RadixUiSelect.ScrollDownButton />
         </S.SelectContent>
       </RadixUiSelect.Portal>
     </S.Select>
   );
-};
-
-//when using Select, selecting item causes to also click behind the item and can trigger unwanted button clicks
-const useSelectInteractionFix = (selectors: string) => {
-  const timeoutRef = useRef<number | undefined>();
-
-  if (typeof document === 'undefined') {
-    return;
-  }
-
-  const root = document.querySelector<HTMLElement>(selectors);
-
-  if (!root) {
-    throw new Error('Root was not found');
-  }
-
-  const disableClicks = () => {
-    root.style.setProperty('pointer-events', 'none');
-  };
-
-  const enableClicks = () => {
-    root.style.removeProperty('pointer-events');
-    // or root.removeAttribute("style") to remove empty attribute.
-  };
-
-  const openChangeHandler = (open: boolean) => {
-    if (open) {
-      clearTimeout(timeoutRef.current);
-      disableClicks();
-    } else {
-      // Casting it here because Node is returning `Timeout` and this handler will run in the browser.
-      timeoutRef.current = setTimeout(enableClicks, 50) as unknown as number;
-    }
-  };
-
-  return openChangeHandler;
 };
