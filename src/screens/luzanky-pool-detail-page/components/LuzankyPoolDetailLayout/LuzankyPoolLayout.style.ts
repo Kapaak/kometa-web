30a917dfd467a6c5940d73b234c522e4a12dbc8b
Pavel Zapletal <pavel.kapaak@gmail.com>
2025-05-08 20:22:37 +0200

    fix: overflowing breadcrumbs

--- src/screens/luzanky-pool-detail-page/components/LuzankyPoolDetailLayout/LuzankyPoolLayout.style.ts
+++ src/screens/luzanky-pool-detail-page/components/LuzankyPoolDetailLayout/LuzankyPoolLayout.style.ts
@@ -1,5 +1,13 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { minBreakpoint } from '~/utils/dimensions';
 
 export const BreadcrumbsWrapper = styled.div`
-  padding: 4rem 2rem;
+  padding: 2rem 2rem 0 2rem;
+  max-width: 100vw;
+
+  ${({ theme }) => css`
+    @media (${minBreakpoint(theme.breakpoints.md)}) {
+      padding: 4rem 2rem;
+    }
+  `}
 `;
