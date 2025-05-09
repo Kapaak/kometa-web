ac65f8a3caba8579f3ecc7dbdc9aa6ad678d5491
Pavel Zapletal <pavel.kapaak@gmail.com>
2025-05-09 17:10:14 +0200

    feat: ui input component

---
+++ src/ui/components/atoms/Input/Input.tsx
@@ -0,0 +1,23 @@
+import * as S from './Input.style';
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  errorMessage?: string;
+  showError?: boolean;
+  label?: string;
+}
+
+export function Input({
+  label,
+  errorMessage,
+  showError,
+  ...props
+}: InputProps) {
+  return (
+    <S.InputContainer>
+      <S.Input {...props} />
+      <S.Label>{label}</S.Label>
+      {showError && <S.ErrorMessage>{errorMessage}</S.ErrorMessage>}
+    </S.InputContainer>
+  );
+}
