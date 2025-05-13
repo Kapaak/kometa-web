ca6c5e1431ab3e494e5fa91eb41d07ea377ef8f5
Pavel Zapletal <pavel.kapaak@gmail.com>
2025-05-13 13:08:13 +0200

    fix: gap in form headline

--- src/screens/pool-application-page/components/FormItems/FormItems.style.ts
+++ src/screens/pool-application-page/components/FormItems/FormItems.style.ts
@@ -1,9 +1,11 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { minBreakpoint } from '~/utils/dimensions';
 
 export const FormContainer = styled.div`
   display: grid;
   gap: 2.4rem;
   grid-template-columns: repeat(auto-fit, minmax(30rem, 1fr));
+  margin: 2rem 0;
 `;
 
 export const FormColumn = styled.div`
@@ -11,6 +13,12 @@ export const FormColumn = styled.div`
   flex-direction: column;
   gap: 1.6rem;
   padding: 1rem 0;
+
+  ${({ theme }) => css`
+    @media (${minBreakpoint(theme.breakpoints.md)}) {
+      gap: 2.4rem;
+    }
+  `}
 `;
 
 export const FormColumnItems = styled.div`
