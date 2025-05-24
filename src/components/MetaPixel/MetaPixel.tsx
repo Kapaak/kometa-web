eae9b81953b84e6158214c97253e61e41a0fc8cd
Pavel Zapletal <pavel.kapaak@gmail.com>
2025-05-24 13:27:04 +0200

    fix: meta pixel

--- src/components/MetaPixel/MetaPixel.tsx
+++ src/components/MetaPixel/MetaPixel.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 import Script from 'next/script';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { MetaPixelManager } from '~/utils/meta-pixel';
 
 const pageview = () => {
@@ -8,33 +8,25 @@ const pageview = () => {
 };
 
 export function MetaPixel() {
+  const [loaded, setLoaded] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
+    if (!loaded) return;
+
     // the below will only fire on route changes (not initial load - that is handled in the script below)
     router.events.on('routeChangeComplete', pageview);
     return () => {
       router.events.off('routeChangeComplete', pageview);
     };
-  }, [router.events]);
+  }, [loaded, router.events]);
 
   return (
     <Script
       id="fb-pixel"
-      dangerouslySetInnerHTML={{
-        __html: `
-  !function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', ${process.env.NEXT_PUBLIC_METAPIXEL_KEY});
-  fbq('track', 'PageView');
-  `,
-      }}
+      onLoad={() => setLoaded(true)}
+      src="/scripts/pixel.js"
+      data-pixel-id={process.env.NEXT_PUBLIC_METAPIXEL_KEY}
     />
   );
 }
