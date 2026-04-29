72f57779bcffc5ee9de523b69ec86882c81cdf1c
Pavel Zapletal <pavel.kapaak@gmail.com>
2026-04-29 20:56:12 +0800

    feat: upgrade eslint version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,75 +1,81 @@
-import nextTypescript from "eslint-config-next/typescript";
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
+import nextTypescript from 'eslint-config-next/typescript';
 
-export default [...nextTypescript, {
-  ignores: ['next-env.d.ts', 'next.config.js'],
-}, ...nextCoreWebVitals, {
-  files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx', '**/*.style.ts'],
-  rules: {
-    'import/no-cycle': 2,
-    'import/order': [
-      'warn',
-      {
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-        'newlines-between': 'always',
-        groups: [
-          'internal',
-          'builtin',
-          'external',
-          ['parent', 'sibling', 'index'],
-          'object',
-          'unknown',
-          'type',
-        ],
-        pathGroups: [
-          {
-            pattern: 'react*',
-            group: 'builtin',
-          },
-          {
-            pattern: 'next*',
-            group: 'builtin',
-          },
-          {
-            pattern: 'next/**',
-            group: 'builtin',
-          },
-          {
-            pattern: '~/public/**',
-            group: 'internal',
-            position: 'after',
-          },
-          {
-            pattern: '~/**',
-            group: 'external',
-            position: 'after',
-          },
-          {
-            pattern: './parts',
-            group: 'object',
-            position: 'after',
-          },
-          {
-            pattern: './*.style',
-            group: 'object',
-            position: 'after',
-          },
-          {
-            pattern: './*.data',
-            group: 'object',
-            position: 'after',
-          },
-          {
-            pattern: './*.type',
-            group: 'object',
-            position: 'after',
-          },
-        ],
-        pathGroupsExcludedImportTypes: ['builtin'],
-      },
-    ],
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  ...nextTypescript,
+  {
+    ignores: ['next-env.d.ts', 'next.config.js'],
   },
-}];
+  ...nextCoreWebVitals,
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx', '**/*.style.ts'],
+    rules: {
+      'import/no-cycle': 2,
+      'import/order': [
+        'warn',
+        {
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+          'newlines-between': 'always',
+          groups: [
+            'internal',
+            'builtin',
+            'external',
+            ['parent', 'sibling', 'index'],
+            'object',
+            'unknown',
+            'type',
+          ],
+          pathGroups: [
+            {
+              pattern: 'react*',
+              group: 'builtin',
+            },
+            {
+              pattern: 'next*',
+              group: 'builtin',
+            },
+            {
+              pattern: 'next/**',
+              group: 'builtin',
+            },
+            {
+              pattern: '~/public/**',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '~/**',
+              group: 'external',
+              position: 'after',
+            },
+            {
+              pattern: './parts',
+              group: 'object',
+              position: 'after',
+            },
+            {
+              pattern: './*.style',
+              group: 'object',
+              position: 'after',
+            },
+            {
+              pattern: './*.data',
+              group: 'object',
+              position: 'after',
+            },
+            {
+              pattern: './*.type',
+              group: 'object',
+              position: 'after',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+        },
+      ],
+    },
+  },
+];
